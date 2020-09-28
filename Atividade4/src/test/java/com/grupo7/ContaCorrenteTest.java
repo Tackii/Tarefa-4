83a3733bdd746f0bc56fa0cb20e1d0a434e3f120
Lucas Pereira Salbego <lucas.salbego99@edu.pucrs.br>
2020-09-27 21:47:03 -0300

    Testes do Salbego

--- Atividade4/src/test/java/com/grupo7/ContaCorrenteTest.java
+++ Atividade4/src/test/java/com/grupo7/ContaCorrenteTest.java
@@ -1,5 +1,6 @@
 package com.grupo7;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,16 +9,75 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ContaCorrenteTest {
-    private ContaCorrente contaCorrente;
+    private ContaCorrente OnPoint;
+    private ContaCorrente OffPoint;
 
     @BeforeEach
     public void setup(){
-        contaCorrente = new ContaCorrente("0000", "Ryan");
+        OnPoint = new ContaCorrente("0000", "Ryan");
+        OffPoint = new ContaCorrente("0001", "Xandao");
     }
 
     @Test
     public void depositoValorNegativo() {        
-        assertFalse(contaCorrente.deposito(-50));
+        assertFalse(OffPoint.deposito(-50));
+    }
+
+    @Test
+    public void depositoGold(){
+        OnPoint.deposito(50519);
+        OnPoint.deposito(148000); //199,999
+        assertEquals(OnPoint.getCategoria(), "Gold");
+        OnPoint.deposito(1); //200,000
+        assertEquals(OnPoint.getCategoria(), "Platinum");
+
+        OffPoint.deposito(199000);
+        assertEquals(OffPoint.getCategoria(), "Gold");
+        OffPoint.deposito(10000000);
+        assertEquals(OffPoint.getCategoria(), "Platinum");
+    }
+
+    @Test
+    public void retiradaSilver(){
+        OnPoint.deposito(1000);
+        OnPoint.retirada(1000);
+        assertEquals(OnPoint.getCategoria(), "Silver");
+
+        OffPoint.deposito(1000);
+        OffPoint.retirada(525);
+        assertEquals(OffPoint.getCategoria(), "Silver");
+    }
+
+    @Test
+    public void retiradaGold(){
+        OnPoint.deposito(50000);
+        OnPoint.retirada(25000);
+        assertEquals(OnPoint.getCategoria(), "Gold");
+        OnPoint.retirada(1);
+        assertEquals(OnPoint.getCategoria(), "Silver");
+
+        OffPoint.deposito(100000);
+        OffPoint.retirada(32656);
+        assertEquals(OffPoint.getCategoria(), "Gold");
+        OffPoint.retirada(67322);
+        assertEquals(OffPoint.getCategoria(), "Silver");
+    }
+
+    @Test
+    public void retiradaPlatinum(){
+        OnPoint.deposito(50000);
+        OnPoint.deposito(150000);//201,500
+        OnPoint.retirada(101500);//100,000
+        assertEquals(OnPoint.getCategoria(), "Platinum");
+        OnPoint.retirada(1);//99,000
+        assertEquals(OnPoint.getCategoria(), "Gold");
+
+        OffPoint.deposito(50000);
+        OffPoint.deposito(200000);
+        OffPoint.retirada(45000);
+        assertEquals(OffPoint.getCategoria(), "Platinum");
+        OffPoint.retirada(180239);
+        assertEquals(OffPoint.getCategoria(), "Gold");
     }
    
 
